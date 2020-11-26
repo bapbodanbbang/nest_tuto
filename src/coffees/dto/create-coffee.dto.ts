7488467e9a60384f6931f509ef96b8aabb5522d9
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-11-27 04:04:28 +0900

    make PartialType && ValidationPipe

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,13 @@
+import { IsString } from 'class-validator';
+
+//request bodt에 유효하지 않은 값이 들어오게 된다면 400 코드를 반환한다.
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
-  readonly flavor: string[];
+
+  @IsString({ each: true })
+  readonly flavors: string[];
 }
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,14 @@
+//nest i @nestjs/mapped-types를 사용하면 반복적인 타입 설정을 피해서 할 수 있다.
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
+
+/*
+일반적인 방식
 export class UpdateCoffeeDto {
   readonly name?: string;
   readonly brand?: string;
   readonly flavor?: string[];
 }
+*/
